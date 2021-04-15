70becb07a5ca3416489c233e7de0b040b5e4c0d4
Darrell Mayson <mcdoob@gmail.com>
2021-04-16 11:05:09 +1200

    create initial static pages controller, views and routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,12 @@
 Rails.application.routes.draw do
-  get 'static_pages/landing_page'
-  get 'static_pages/privacy_policy'
+  
+  root 'static_pages#landing_page'
+  
+  # get 'static_pages/landing_page'
+  # get 'static_pages/privacy_policy'
+
+  # Remove need to use "../static_pages/[TARGET PAGE]"
+  get 'privacy_policy', to: 'static_pages#privacy_policy'
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
