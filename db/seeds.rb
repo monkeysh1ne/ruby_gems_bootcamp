65a7c76c3362faeb31c8f8ae0fdcaa3e4fde913f
Darrell Mayson <mcdoob@gmail.com>
2021-04-20 06:28:47 +1200

    seeded db with courses created admin@example.com user

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,10 +6,16 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
+
+# Once establishing relationships/associations between Users and Courses
+# User.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password')
+
+
 # seed Course db
 30.times do 
   Course.create!([
     title: Faker::Educator.course_name,
-    description: Faker::TvShows::GameOfThrones.quote
+    description: Faker::TvShows::GameOfThrones.quote,
+    user_id: User.first.id
   ])
 end
